3f9aa42f04e2caa9fd57c5108b8d26e70ccd24f0
maria <mpramos78915400@gmail.com>
2024-11-05 20:25:58 -0400

    index

---
+++ javascript-repaso/index5.js
@@ -0,0 +1,15 @@
+
+console.log('hola');
+setTimeout(() => {
+    console.log('hola 👋');
+    
+}, 5000);
+console.log('como estas');
+console.log('espero que bien');
+
+
+const ordenTareas=async()=>{
+    let tarea1= await obtnerData()
+    let tarea2= await tarea1.obtData()
+
+}
